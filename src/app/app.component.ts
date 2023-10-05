54cfa4a15eab87de8d4d0024d9f9acd7d0272eb7
Lautaro Avila <lavila@eprern.gov.ar>
2023-10-05 09:33:04 -0300

    Modificaciones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,37 @@
 import { Component } from '@angular/core';
+import { ServiveService } from './servive.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers:[ServiveService]
 })
 export class AppComponent {
   title = 'PruebaSimpleFront';
+constructor(private servive:ServiveService){}
 
+  name:string = ''
+  successMessage : string = ''
 
-  create(){
-    
+  create() {
+    // Verifica si el campo "name" está lleno
+    if (this.name.trim() !== '') {
+      // Crea un objeto con el campo "name"
+      const data = { name: this.name };
+
+      // Llama al servicio para enviar la solicitud POST
+      this.servive.create(data).subscribe(
+        (response) => {
+          // Maneja la respuesta del servidor
+          this.successMessage = 'Registro exitoso';
+          // Puedes realizar otras acciones aquí si es necesario
+        },
+        (error) => {
+          // Maneja los errores si es necesario
+          console.error('Error:', error);
+        }
+      );
+    }
   }
 }
--- src/app/servive.service.ts
+++ src/app/servive.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { SimpleObkect } from './simpleObject';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,11 +13,16 @@ simpleObject!: SimpleObkect
   constructor(private http:HttpClient) { }
   private API_CREATE: string = "http://localhost:8080/restful/services/simple.SimpleObjects/actions/create/invoke";
 
-  create(){
+  
+
+  
+
+  create(data:any):Observable<any>{
     const headers = new HttpHeaders();
 
-    const url = `${this.API_CREATE}`;
-    return this.http.post(url,this.simpleObject, { headers, responseType: 'text' as 'json' });
+    // const url = `${this.API_CREATE}`;
+    // return this.http.post(url,{ headers, responseType: 'text' as 'json' });
+    return this.http.post(this.API_CREATE,data, { headers, responseType: 'text' as 'json' });
   }
   
 }
